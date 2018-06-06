91244bafecf712430747b4dbc166e9d2bfdf84e8
Ryan Yogan <ryanyogan@gmail.com>
2018-06-06 15:42:37 -0700

    Added in voyager for viz of graph, fixed a silly typo from a stash, whoops

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { graphiqlExpress } from 'apollo-server-express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 import setupMiddleware from './middleware';
 import { connect } from './db';
 import { graphQLRouter } from './api';
@@ -17,6 +18,7 @@ app.use(
     endpointURL: config.graphiql.endpointURL,
   }),
 );
+app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
 
 app.use('*', (req, res) =>
   res.json({ message: 'Please use /graphql when connecting' }),
