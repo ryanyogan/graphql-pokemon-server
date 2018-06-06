6087c01581b0e1d84ab23b5072e854ae0e81b1da
Ryan Yogan <ryanyogan@gmail.com>
2018-06-05 18:38:49 -0700

    WIP - Input Types

--- src/api/resources/pokemon/pokemon.resolvers.js
+++ src/api/resources/pokemon/pokemon.resolvers.js
@@ -4,12 +4,10 @@ const getOnePokemon = (_, { id }) => Pokemon.findById(id).exec();
 
 const getAllPokemons = () => Pokemon.find({}).exec();
 
-const createPokemon = (_, { input: { name, imageUrl, classification } }) =>
-  Pokemon.create({
-    imageUrl,
-    name,
-    classification,
-  });
+const createPokemon = (_, { input }) => {
+  console.log('INPUT', input);
+  return Pokemon.create(input);
+};
 
 export const pokemonResolvers = {
   Query: {
